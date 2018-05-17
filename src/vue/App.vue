3294542eaacd4f984bb8825f763007d3a93f8a17
slim <smedini@gmail.com>
2018-05-17 16:55:21 +0200

    v2

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -2,102 +2,95 @@
     <div id="app">
         <v-app light>
             <v-layout column>
-                <center><t-play style="max-width: 80em;" :film="intro"></t-play></center>
+                <t-play style="max-width: 80em;margin-left: auto;margin-right:auto" :film="intro"></t-play>
                 <transition name="slide-y-reverse-transition">
                     <v-flex v-if="introEnd">
-                        <div class="phrase">Nos achats sont de vrais bulletins de vote.</div>
-                        <div class="phrase">Et si on savait ce qui se cache derrière nos produits ?</div>
-                        <div class="phrase">Et si les fabricants pouvaient montrer ce qu'ils font pour la planète ?</div>
-                        <div class="phrase">Et si on pouvait proposer aux fabricants les changements que l'on veut ?</div>
-
+                        <div class="phrase">Nos achats sont de vrais bulletins de vote...</div>
                         <br>
                         <br>
+                        <br>
+                        <div class="phrase">Et si on voyait ce qui se cache derrière nos produits ?</div>
                         <br>
                         <br>
                         <img class="logo" src="logo/foret.svg"/>
-
-                        <!--<div class="phrase">Qu'est ce qui, dans mes achats, est plus ou moins "propre" ?</div>-->
-
-                        <!--<div class="phrase">La bonne façon d'acheter</div>-->
-                        <!--<div class="phrase">La bonne façon de vendre</div>-->
-                        <!--<div class="phrase">Mieux faire, mieux vivre.</div>-->
-                        <!--<div class="phrase">Construire le marché économique de demain</div>-->
+                        <br>
+                        <br>
+                        <br>
+                        <br>
                         <!--<br>-->
                         <!--<br>-->
-                        <!--<br>-->
-                        <!--<div class="phrase">Avoir de vraies informations avant d'acheter</div>-->
-                        <!--<div class="phrase">Connaître, dans mes achats, ce qui ne semble pas écologique et qui pourtant l'est</div>-->
-                        <!--<div class="phrase">Connaître, dans mes achats, ce qui semble écologique et qui pourtant ne l'est pas</div>-->
-                        <!--<div class="phrase">Me faire une idée de l'impact de mes choix</div>-->
-                        <!--<br>-->
-                        <!--<br>-->
-                        <!--<br>-->
-                        <!--<div class="phrase">Vendre en amortissant mes décisions écologiques</div>-->
-                        <!--<div class="phrase">Vendre en reportant les qualités écologiques de mes fournisseurs</div>-->
-                        <!--<div class="phrase">Créer un marché dynamique et durable, imitant la nature</div>-->
-                        <!--<div class="phrase">Visualiser les ressources nécessaires à la confection d'un produit</div>-->
-                        <!--<div class="phrase">Accorder créateurs de déchets, acheteurs de matières premières, et experts en réutilisation</div>-->
-                        <!--<div class="phrase">Demander des informations au sujet d'un produit</div>-->
-                        <!--<div class="phrase">Booster mon entreprise en lui apportant de la transparence</div>-->
-                        <!--<div class="phrase">Proposer des idées de changement: la R&D publique</div>-->
-                        <!--<div class="phrase">Recueillir des idées de changement: la R&D publique</div>-->
-                        <!--<div class="phrase">Comparer les ressources et les déchets de produits</div>-->
-                        <!--<div class="phrase">Visualisez les faits grâce à des équivalences pertinentes</div>-->
-                        <!--<div class="phrase">Construisez vos propres comparaisons environnementales</div>-->
-                        <!--<div class="phrase">Créer des produits ou des compositions de services à partir de produits existants</div>-->
-                        <!--<div class="phrase">Apporter mon expertise en documentant ou en indiquant les sources attestant une information</div>-->
-                        <!--<div class="phrase">Diffuser et pérenniser une étude environnementale en la connectant au reste du monde</div>-->
+                        <!--<div class="phrase">Et si les fabricants pouvaient montrer ce qu'ils font pour la planète ?</div>-->
+                        <!--<div class="phrase">Et si on pouvait proposer aux fabricants les changements que l'on veut ?</div>-->
+                        <!--<img class="logo" src="logo/produits.svg"/>-->
                         <br>
                         <br>
                         <br>
                         <br>
-
-                        <div class="phrase">BlueForest est unique</div>
-                        <div class="pphrase">Nous, particuliers, associations, entreprises, chercheurs, autorités publique, pouvons y intégrer nos informations sur les matières premières, l'énergie, les déchets...</div>
-                        <div class="pphrase">Et ainsi construire une base de connaissances inédite et utilisable par tout le monde. Par exemple:</div>
-
+                        <br>
+                        <br>
+                        <v-carousel style="max-width: 40em;min-width:20em;height:auto;margin-left: auto;margin-right:auto" hide-delimiters>
+                            <h1>On trouve 100mg de vitamines C dans...</h1>
+                            <v-carousel-item>
+                                <img class="logo" src="img/pomme.svg" style="max-width:50%"/>
+                                <v-toolbar>
+                                    <v-toolbar-title>1 Pomme, en 1950</v-toolbar-title>
+                                </v-toolbar>
+                            </v-carousel-item>
+                            <v-carousel-item>
+                                <img class="logo" src="img/pommes.svg" style="max-width:50%"/>
+                                <v-toolbar>
+                                    <v-toolbar-title>10kg de pommes, aujourd'hui</v-toolbar-title>
+                                </v-toolbar>
+                            </v-carousel-item>
+                            <v-carousel-item>
+                                <br>
+                                <br>
+                                <br>
+                                <div class="phrase">Vous connaissez des choses pointues en vitamine C ?</div>
+                                <br>
+                                <br>
+                                <div class="phrase">Ajoutez-les sur BlueForest</div>
+                                <br>
+                                <br>
+                                <br>
+                                <br>
+                            </v-carousel-item>
+                        </v-carousel>
+                        <!--<div class="phrase">Un filet d'espadon a nécessité 200kg de phyto-plancton.</div>-->
                         <br>
                         <br>
                         <br>
                         <br>
-
-                        <div class="phrase">Pour le co2, acheter 1kg de tomate en hiver revient à acheter 1 tonne de tomate en été.</div>
-                        <div class="phrase">Il y avait autant de vitamines C dans 1 pomme en 1950 que dans 20kg de pommes aujourd'hui.</div>
-                        <div class="phrase">200kg de phyto-plancton absorbe autant de co2 en un an qu'en émet une voiture en 15 minutes.</div>
-                        <div class="phrase">Un filet d'espadon a nécessité 200kg de phyto-plancton.</div>
                         <br>
-                        <br>
-                        <img class="logo" src="logo/produits.svg"/>
-                        <br>
-                        <br>
-                        <div class="phrase">Ensemble, élevons le niveau de la discussion en matière d'environnement.</div>
+                        <div class="phrase">Ensemble, élevons le niveau de la discussion.</div>
                         <div class="phrase">Donnons-nous les moyens de mieux choisir sans se priver.</div>
                         <br>
                         <br>
-                        <center>
-
-                            <v-card v-if="done" transition="scale-transition" origin="center center">
-                                <t-play :film="outro"></t-play>
-                            </v-card>
-                            <v-card v-else style="max-width: 40em;min-width:20em">
-                                <v-toolbar card dark color="primary">
-                                    <v-btn flat icon @click="egg = true"><v-icon large>add_alert</v-icon></v-btn>
-                                    <v-toolbar-title v-if="egg">Que faire en premier?</v-toolbar-title>
-                                </v-toolbar>
-                                <v-container fluid>
-                                    <v-form v-model="valid" v-on:submit.prevent="validate" ref="form">
-                                        <v-text-field type="mail" label="mail" v-model="mail" required :rules="[rules.required, rules.email]"/>
-                                        <v-text-field type="text" label="commentaire" v-model="message" multi-line required :rules="[rules.required, rules.minlength, rules.maxlength]" counter="250"/>
-                                    </v-form>
-                                </v-container>
-                                <v-card-actions>
-                                    <v-btn block @click="validate" large color="green">
-                                        <v-icon x-large color="white">check</v-icon>
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </center>
+                        <v-card v-if="done" transition="scale-transition" origin="center center" style="margin-left: auto;margin-right:auto">
+                            <t-play :film="outro"></t-play>
+                        </v-card>
+                        <v-card v-else style="max-width: 40em;min-width:20em;margin-left: auto;margin-right:auto">
+                            <v-toolbar card dark color="primary">
+                                <v-icon large>add_alert</v-icon>
+                                <v-toolbar-title>Intéressé par la suite?</v-toolbar-title>
+                            </v-toolbar>
+                            <v-container fluid>
+                                <v-form v-model="valid" v-on:submit.prevent="validate" ref="form">
+                                    <v-text-field type="mail" label="mail" v-model="mail" required :rules="[rules.required, rules.email]"/>
+                                    <v-text-field type="text" label="commentaire" v-model="message" multi-line required :rules="[rules.required, rules.minlength, rules.maxlength]" counter="250"/>
+                                </v-form>
+                            </v-container>
+                            <v-card-actions>
+                                <v-btn block @click="validate" large color="green">
+                                    <v-icon x-large color="white">check</v-icon>
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
                         <br>
+                        <br>
+                        <br>
+                        <br>
+                        <img class="logo" src="logo/produits.svg" style="min-width:20em;height:auto"/>
                         <br>
                         <br>
                         <br>
@@ -131,13 +124,13 @@
                     } else {
                         this.introEnd = true;
                     }
-                });
+                }).catch(() => this.introEnd = true);
             getFilmByName(outroTitle)
                 .then(f => this.outro = f);
         },
         data: function () {
             return {
-                egg:null,
+                egg: null,
                 intro: null,
                 outro: null,
                 introEnd: false,
